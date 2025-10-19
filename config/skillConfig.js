1ca71117b673887e2c2153f9d8ae0753d506c2ff
hyacinth <905149301@qq.com>
2025-10-20 03:21:00 +0800

    修改版本号

--- config/skillConfig.js
+++ config/skillConfig.js
@@ -10,7 +10,7 @@ export const skillConfig = [
     { name: '七星拱瑞', description: '使对手下次攻击无法施展且无法触发技能(但有30%概率抵抗成功)', freezeChance: 0.7, chance: 0.18, cooldown: 4 },
     { name: '定军', description: '强制对手攻击自己,无法触发技能(持续2次攻击)', taunt: true, turns: 2, chance: 0.19, cooldown: 3 },
     { name: '金钟罩', description: '对手下次攻击时反弹伤害的20%给对手', damageReflection: 0.2, turns: 1, chance: 0.23, cooldown: 2 },
-    { name: '紫气东来', description: '增加自身10%会心几率(持续2次攻击,可叠加)', critChanceBoost: 0.1, turns: 2, chance: 0.18, cooldown: 1 },
+    { name: '紫气东来', description: '增加自身20%会心几率(持续2次攻击,可叠加)', critChanceBoost: 0.2, turns: 2, chance: 0.18, cooldown: 1 },
     { name: '阳春白雪', description: '获得一个最大生命值20%的护盾(可叠加)', shieldAmount: 0.2, chance: 0.15, cooldown: 3 },
     { name: '灭影追风', description: '降低对手10点攻击力(持续2次攻击,可叠加)', attackReduction: 10, turns: 2, chance: 0.19, cooldown: 2 },
     { name: '盾壁', description: '提升50%招架率,对手攻击有概率招架,不受到伤害(持续2次攻击,可叠加)', parryBoost: 0.5, turns: 2, chance: 0.18, cooldown: 2 }
